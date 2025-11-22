7ae4d9a2f09bc435da9c908c62bcb91e8b5fc3e6
Isamu Mogi <isamu@leafytree.jp>
2025-11-23 03:17:31 +0900

    chore: replace print_path_walk_error function with inline lambda for directory walk error handling

--- devcontainer_fixup_workspace_permissions.py
+++ devcontainer_fixup_workspace_permissions.py
@@ -31,10 +31,6 @@ else:
 logger = logging.getLogger(__name__)
 
 
-def print_path_walk_error(warning_messages: list[str], os_error: OSError) -> None:
-    warning_messages.append(f"Failed to walk directories: {os_error}")
-
-
 def fixup_directory_owner_and_permission(
     directory: Path, warning_messages: list[str], *, uid: int, gid: int, umask: int
 ) -> None:
@@ -88,7 +84,9 @@ def fixup_files(
     all_file_paths: list[Path] = [workspace_path]
     for root, directory_names, file_names in os.walk(
         workspace_path,
-        onerror=functools.partial(print_path_walk_error, warning_messages),
+        onerror=lambda error: warning_messages.append(
+            f"Failed to walk directories: {error}"
+        ),
     ):
         root_path = Path(root)
         progress.update()
